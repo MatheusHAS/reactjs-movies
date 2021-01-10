cffced9ec7f9c7ad798e36926eaabec934e479d6
Matheus Azambuja <matheushenrique.ads@gmail.com>
2021-01-09 22:23:35 -0300

    feat: add infinite scroll to load more items on homepage

--- src/presentation/pages/home/Home.tsx
+++ src/presentation/pages/home/Home.tsx
@@ -6,9 +6,10 @@ import { Link } from 'react-router-dom'
 import * as MoviesActions from '@/main/store/containers/movies/actions'
 import { MovieListItem, SkeletonList } from '@/presentation/components'
 import { Container, Posters, Title } from './Styles'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 interface Props {
-  loadRequest: () => void
+  loadRequest: (page: number) => void
   movieListState: IMovieListState
 }
 
@@ -23,7 +24,7 @@ const Home: React.FC<Props> = ({ loadRequest, movieListState }: Props) => {
 
   useEffect(() => {
     if (!movies || movies.length === 0) {
-      loadRequest()
+      loadRequest(1)
     }
   }, [])
 
@@ -33,23 +34,35 @@ const Home: React.FC<Props> = ({ loadRequest, movieListState }: Props) => {
     </Link>
   )
 
+  const HomeListSkeleton = () => (
+    <SkeletonList
+      count={8}
+      height={300}
+      width={200}
+      bgColor={'#1d1f35'}
+      highlightColor={{
+        red: 40,
+        green: 39,
+        blue: 93,
+      }}
+    />
+  )
+
   return (
     <Container>
       <Title>Filmes populares</Title>
       {movieListState.loading ? (
-        <SkeletonList
-          count={8}
-          height={300}
-          width={200}
-          bgColor={'#1d1f35'}
-          highlightColor={{
-            red: 40,
-            green: 39,
-            blue: 93,
-          }}
-        />
+        <HomeListSkeleton />
       ) : (
-        <Posters>{movies && movies.map((movie) => renderMovieItem(movie))}</Posters>
+        <InfiniteScroll
+          hasMore={movieListState.page < movieListState.totalPages}
+          dataLength={movieListState.totalPages}
+          loader={<HomeListSkeleton />}
+          style={{ overflow: 'hidden' }}
+          next={() => loadRequest(movieListState.page + 1)}
+        >
+          <Posters>{movies && movies.map((movie) => renderMovieItem(movie))}</Posters>
+        </InfiniteScroll>
       )}
     </Container>
   )
