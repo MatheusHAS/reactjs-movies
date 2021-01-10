ad3b68e7048084ec96e0e6164a70d818ac1b5e8f
Matheus Azambuja <matheushenrique.ads@gmail.com>
2021-01-09 22:32:47 -0300

    fix: fix console errors on rating component

--- src/presentation/components/rating/Rating.tsx
+++ src/presentation/components/rating/Rating.tsx
@@ -6,21 +6,17 @@ interface RatingProps {
 }
 
 export const Rating: React.FC<RatingProps> = ({ value }: RatingProps) => {
-  const star = (color: string | null = null) => {
-    return (
-      <>
-        <Star width={25} height={25} fill={color}>
-          <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" />
-        </Star>
-      </>
-    )
-  }
+  const starWithProps = (color: string = null, key: any) => (
+    <Star key={key} width={25} height={25} fill={color}>
+      <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" />
+    </Star>
+  )
 
   const renderStars = (value: number = 1) => {
     const ratingValue = Math.round(value) / 2
     const arrayItems: any[] = []
     for (let p = 0; p < 5; p++) {
-      arrayItems.push(star(p < ratingValue ? '#ffd055' : null))
+      arrayItems.push(starWithProps(p < ratingValue ? '#ffd055' : null, `rating-${p}`))
     }
     return arrayItems
   }
